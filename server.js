44301e6c836d017625429d8a9b06d425cc71e682
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-04 13:42:10 -0800

    Add home.ejs and render it

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("The server is running!");
+  res.render("home.ejs");
 });
 
 app.listen(3000, () => {
